94e97ad670cb3ee51e180996a85dc523e3b72dcf
Berk Kırtay <berkberk24@gmail.com>
2022-02-15 01:50:40 +0300

    CI test err fix

--- src/Blockchain/BlockchainLogger.py
+++ src/Blockchain/BlockchainLogger.py
@@ -6,11 +6,11 @@ import sys
 
 def initializeLogger():
     logger_level = logging.CRITICAL
-    with open("logger_config.json", 'r') as f:
-        config = json.load(f)
+    # with open("logger_config.json", 'r') as f:
+    #   config = json.load(f)
 
-    if config["logging"] == 1:
-        logger_level = logging.INFO
+   # if config["logging"] == 1:
+    logger_level = logging.INFO
 
     logging.basicConfig(filename='blockchain.log',
                         encoding='utf-8',
@@ -18,7 +18,7 @@ def initializeLogger():
                         format='%(asctime)s [%(levelname)s] %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')  # %(filename)s:%(lineno)s
 
-    if config["log_cli"] == 1:
-        logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+   # if config["log_cli"] == 1:
+    #    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
     logging.info("-------New logging session is initialized-------\n")
