eb9ef27db45ac82829527503163f74565a116a09
Berk Kırtay <berkberk24@gmail.com>
2022-02-15 12:22:54 +0300

    Test coverage

--- src/Blockchain/Unit_tests.py
+++ src/Blockchain/Unit_tests.py
@@ -195,3 +195,43 @@ def test_shouldRaiseErrWhenAttemptToUseDifferentDataType():
 
     assert TransactionDataConflictError().err_str in str(
         err.value)
+
+
+def test_integration_blockchainDataIO():
+    blockchain = Blockchain(3, 10)
+
+# Creating random wallets
+    numberofWallets = 5
+
+    wallets = []
+    for i in range(numberofWallets):
+        newWallet = Wallet("person")
+        wallets.append(newWallet)
+        blockchain.forceTransaction(newWallet.publicKey, 100000000)
+
+    blockchain.handleTransaction(wallets[0].publicKey)
+
+    for i in range(1, 7):
+        randomWallet1 = wallets[random.randint(0, numberofWallets - 1)]
+        randomWallet2 = wallets[random.randint(0, numberofWallets - 1)]
+        blockchain.addTransaction(Transaction(
+            randomWallet1.publicKey, randomWallet2.publicKey, random.randint(1, 1000), randomWallet1.privateKey))
+
+    blockchain.handleTransaction(wallets[0].publicKey)
+
+# Blockchain data export and import
+    usersBalanceBeforeExport = 0
+    usersBalanceAfterExport = 0
+
+    for i in range(numberofWallets):
+        wallets[i].updateTransactions(blockchain)
+        usersBalanceBeforeExport += wallets[i].getBalance(blockchain)
+
+    BlockDataIO().exportData(blockchain, "blockchainData.json")
+    blockchain2 = BlockDataIO().readDataAndImport("blockchainData.json")
+
+    for i in range(numberofWallets):
+        wallets[i].updateTransactions(blockchain2)
+        usersBalanceAfterExport += wallets[i].getBalance(blockchain2)
+
+    assert usersBalanceBeforeExport == usersBalanceAfterExport
